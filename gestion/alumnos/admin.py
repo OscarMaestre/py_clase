748d782bbe050dab434b70f5cdc458920f197e52
Oscar GG <profesor.oscar.gomez@gmail.com>
2017-06-20 15:45:35 +0200

    Modelos fallan

--- gestion/alumnos/admin.py
+++ gestion/alumnos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(Alumno)
+admin.site.register(Examen)
+admin.site.register(AlumnoRealizaExamen)
+admin.site.register(Modulo)
+admin.site.register(AlumnoCursaModulo)
--- gestion/alumnos/models.py
+++ gestion/alumnos/models.py
@@ -3,16 +3,43 @@ from django.db import models
 # Create your models here.
 
 class Alumno(models.Model):
-    dni             = models.CharField(max_length=11)
-    nombre          = models.CharField(max_length=40)
-    apellidos       = models.CharField(max_length=100)
-    
+    dni             =       models.CharField(max_length=11)
+    nombre          =       models.CharField(max_length=40)
+    apellidos       =       models.CharField(max_length=100)
+    class Meta:
+        ordering = ["apellidos","nombre"]
+    def __str__(self):
+        return self.apellidos + " " + self.nombre
     
 class Modulo(models.Model):
-    codigo_junta    =   models.IntegerField(primary_key=True)
-    nombre          =   models.CharField(max_length=120)
+    codigo_junta    =       models.IntegerField(primary_key=True)
+    nombre          =       models.CharField(max_length=120)
+    def __str__(self):
+        return self.nombre
     
 class AlumnoCursaModulo(models.Model):
     alumno          =       models.ForeignKey(Alumno)
     modulo          =       models.ForeignKey(Modulo)
     repite_modulo   =       models.BooleanField()
+    class Meta:
+        verbose_name_plural = "AlumnosCursanModulo"
+    
+class Examen(models.Model):
+    nombre      =           models.CharField(max_length=90)
+    modulo      =           models.ForeignKey(Modulo)
+    def __str__(self):
+        return str(self.nombre) + " - " + str(self.modulo)
+    class Meta:
+        verbose_name_plural = "Examenes"
+    
+class AlumnoRealizaExamen(models.Model):
+    alumno      =           models.ForeignKey(Alumno, related_name="AlumnoExamen")
+    examen      =           models.ForeignKey(Examen)
+    nota        =           models.DecimalField(max_digits=6, decimal_places=4)
+    
+    def __str__(self):
+        cad=str(self.alumno) + " - " + str(self.examen) + " - Nota:"+str(self.nota)
+        return cad
+    class Meta:
+        verbose_name_plural = "AlumnosRealizanExamen"
+        ordering = ["alumno__apellidos"]
